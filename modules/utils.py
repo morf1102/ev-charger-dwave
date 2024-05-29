dab07d810d202b4ceb2927ec928f02389b05c852
Félix Moras <felix.moras@usherbrooke.ca>
2024-05-29 15:45:14 -0400

    cleaned up imports

--- modules/utils.py
+++ modules/utils.py
@@ -1,24 +1,11 @@
-import os
-from typing import List, Tuple, Optional, Union
+from typing import List, Tuple
 
-import geopandas as gpd
-import pandas as pd
 import numpy as np
 from scipy.spatial.distance import cdist
 
 
-def distance(a: Tuple[int, int], b: Tuple[int, int]) -> int:
-    """
-    Calculate the distance between two points on a grid.
-    """
-    # return cityblock(a, b)
-    return (a[0] ** 2 - 2 * a[0] * b[0] + b[0] ** 2) + (
-        a[1] ** 2 - 2 * a[1] * b[1] + b[1] ** 2
-    )
-
-
 def influence_matrix(
-    x: List[Tuple[int, int]], y: List[Tuple[int, int]], sigma: Union[float, int] = 1
+    x: List[Tuple[int, int]], y: List[Tuple[int, int]], sigma: float = 1.
 ):
     """
     Calculate the gaussian influence matrix between two sets of coordinates.
